87418f770a70d178be1d25ed1a5232663fc6cc34
Kaito Kirigaya <cmrivera353@gmail.com>
2017-01-25 16:14:22 -0600

    Init Commit

---
+++ Assets/Scripts/Entities/Player.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private static Player instance;
+
+	private static int maxHealth;
+	private static int xpToNextLevel;
+	private static int currentLevel;
+	private static int currentHealth;
+	private static int currentXP;
+
+	public Sprite sprite;
+	
+	public static Player Instance {
+		get {
+			if(instance == null) {
+				instance = GameObject.Find("Player").GetComponent<Player>();
+			}
+			return instance;
+		}
+
+		set {
+			instance = value;
+		}
+	}
+
+	public static int MaxHealth {
+		get {
+			if(maxHealth == 0) {
+				maxHealth = 100;
+			}
+			return maxHealth;
+		}
+
+		set {
+			maxHealth = value;
+		}
+	}
+
+	public static int XpToNextLevel {
+		get {
+			return xpToNextLevel;
+		}
+
+		set {
+			xpToNextLevel = value;
+		}
+	}
+
+	public static int CurrentLevel {
+		get {
+			if(currentLevel == 0) {
+				currentLevel = 1;
+			}
+			return currentLevel;
+		}
+
+		set {
+			currentLevel = value;
+		}
+	}
+
+	public static int CurrentHealth {
+		get {
+			return currentHealth;
+		}
+
+		set {
+			currentHealth = value;
+		}
+	}
+
+	public static int CurrentXP {
+		get {
+			return currentXP;
+		}
+
+		set {
+			currentXP = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	private static GameObject pauseMenu;
+
+	public static GameObject PauseMenu {
+		get {
+			if(pauseMenu == null) {
+				pauseMenu = GameObject.Find("Pause Menu");
+				pauseMenu.SetActive(false);
+			}
+			return pauseMenu;
+		}
+
+		set {
+			pauseMenu = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		PauseMenu.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void togglePauseMenu(bool toggle) {
+		PauseMenu.SetActive(toggle);
+	}
+}
