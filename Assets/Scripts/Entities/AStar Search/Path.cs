0fb5a0aa3348660b1c16b310ab0eff2f7f0b2607
Kaito Kirigaya <cmrivera353@gmail.com>
2017-02-02 16:16:13 -0600

    a* kinda works now rotation needs to be fixed

---
+++ Assets/Scripts/Entities/AStar Search/Path.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path {
+	public readonly Vector2[] lookPoints;
+	public readonly Line[] turnBoundaries;
+	public readonly int finishLineIndex;
+
+	public Path(Vector2[] waypoints, Vector2 startPos, float turnDist) {
+		lookPoints = waypoints;
+		turnBoundaries = new Line[lookPoints.Length];
+		finishLineIndex = turnBoundaries.Length - 1;
+
+		Vector2 previousPoint = startPos;
+		for(int i = 0; i < lookPoints.Length; i++) {
+			Vector2 currentPoint = lookPoints[i];
+			Vector2 dirToCurrentPoint = (currentPoint - previousPoint).normalized;
+			Vector2 turnBoundaryPoint = i == finishLineIndex ? currentPoint : currentPoint - dirToCurrentPoint * turnDist;
+			turnBoundaries[i] = new Line(turnBoundaryPoint, previousPoint - dirToCurrentPoint * turnDist);
+			previousPoint = turnBoundaryPoint;
+		}
+	}
+
+	public void DrawWithGizmos() {
+		Gizmos.color = Color.black;
+
+		foreach(Vector2 p in lookPoints) {
+			Gizmos.DrawCube(p, Vector2.one);
+		}
+
+		Gizmos.color = Color.white;
+		foreach(Line l in turnBoundaries) {
+			l.DrawWithGizmos(10);
+		}
+	}
+}
--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -4,57 +4,77 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+	const float minPathUpdateTime = .2f;
+	const float pathUpdateMoveThreshold = .5f;
+
 	public Transform target;
 	public float speed = 5;
-	Vector2[] path;
-	int targetIndex;
+	public float turnSpeed = 3;
+	public float turnDist = 5;
 
-	private void Update() {
-		PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+	Path path;
+
+	private void Start() {
+		StartCoroutine(UpdatePath());
 	}
 
-	public void OnPathFound(Vector2[] newPath, bool pathSuccessful) {
+	public void OnPathFound(Vector2[] waypoints, bool pathSuccessful) {
 		if (pathSuccessful) {
-			path = newPath;
-			targetIndex = 0;
+			path = new Path(waypoints, transform.position, turnDist);
+
 			StopCoroutine("FollowPath");
 			StartCoroutine("FollowPath");
 		}
 	}
 
+	IEnumerator UpdatePath() {
+		if (Time.timeSinceLevelLoad < .3f) {
+			yield return new WaitForSeconds(.3f);
+		}
+		PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+		float sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
+		Vector2 targetPosOld = target.position;
 
-	IEnumerator FollowPath() {
-		if (path.Length > 0) {
-			targetIndex = 0;
-			Vector2 currentWaypoint = path[0];
-
-			while (true) {
-				if ((Vector2) transform.position == currentWaypoint) {
-					targetIndex++;
-					if (targetIndex >= path.Length) {
-						yield break;
-					}
-
-					currentWaypoint = path[targetIndex];
-				}
-				transform.position = Vector2.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
-				yield return null;
+		while (true) {
+			yield return new WaitForSeconds(minPathUpdateTime);
+			if (((Vector2) target.position - targetPosOld).sqrMagnitude > sqrMoveThreshold) {
+				PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+				targetPosOld = target.position;
 			}
 		}
 	}
 
-	public void OnDrawGizmos() {
-		if (path != null) {
-			for (int i = targetIndex; i < path.Length; i++) {
-				Gizmos.color = Color.black;
-				Gizmos.DrawCube(path[i], Vector3.one);
+	IEnumerator FollowPath() {
+		bool followingPath = true;
+		int pathIndex = 0;
+		transform.LookAt(path.lookPoints[0]);
 
-				if (i == targetIndex) {
-					Gizmos.DrawLine(transform.position, path[i]);
+		while (true) {
+			Vector2 pos2D = new Vector2(transform.position.x, transform.position.y);
+			while (path.turnBoundaries[pathIndex].HasCrossedLine(pos2D)) {
+				if(pathIndex == path.finishLineIndex) {
+					followingPath = false;
+					break;
 				} else {
-					Gizmos.DrawLine(path[i - 1], path[i]);
+					pathIndex++;
 				}
 			}
+
+			if (followingPath) {
+				Quaternion targetRotation = Quaternion.LookRotation((Vector3) path.lookPoints[pathIndex] - transform.position);
+				transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+				transform.rotation = new Quaternion(0, 0, transform.rotation.z, 1);
+				transform.position = Vector2.MoveTowards(transform.position, path.lookPoints[pathIndex], speed * Time.deltaTime);
+			}
+
+			yield return null;
+		}
+	}
+
+
+	public void OnDrawGizmos() {
+		if (path != null) {
+			path.DrawWithGizmos();
 		}
 	}
 }
