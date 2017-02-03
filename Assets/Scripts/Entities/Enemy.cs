77e8138a9340e589a46b9c4bfed3bee6198ae938
Kaito Kirigaya <cmrivera353@gmail.com>
2017-02-03 13:18:16 -0600

    still need to figure out rotation

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -45,29 +45,35 @@ public class Enemy : MonoBehaviour {
 	}
 
 	IEnumerator FollowPath() {
-		bool followingPath = true;
-		int pathIndex = 0;
-		transform.LookAt(path.lookPoints[0]);
+		if (path.lookPoints.Length > 0) {
+			bool followingPath = true;
+			int pathIndex = 0;
+			transform.LookAt(path.lookPoints[0]);
 
-		while (true) {
-			Vector2 pos2D = new Vector2(transform.position.x, transform.position.y);
-			while (path.turnBoundaries[pathIndex].HasCrossedLine(pos2D)) {
-				if(pathIndex == path.finishLineIndex) {
-					followingPath = false;
-					break;
-				} else {
-					pathIndex++;
+			while (true) {
+				Vector2 pos2D = new Vector2(transform.position.x, transform.position.y);
+				while (path.turnBoundaries[pathIndex].HasCrossedLine(pos2D)) {
+					if (pathIndex == path.finishLineIndex) {
+						followingPath = false;
+						break;
+					} else {
+						pathIndex++;
+					}
 				}
-			}
 
-			if (followingPath) {
-				Quaternion targetRotation = Quaternion.LookRotation((Vector3) path.lookPoints[pathIndex] - transform.position);
-				transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
-				transform.rotation = new Quaternion(0, 0, transform.rotation.z, 1);
-				transform.position = Vector2.MoveTowards(transform.position, path.lookPoints[pathIndex], speed * Time.deltaTime);
-			}
+				if (followingPath) {
+					Quaternion targetRotation = Quaternion.LookRotation((Vector3) path.lookPoints[pathIndex] - transform.position);
+					if (transform.rotation.z > 90 || transform.rotation.z < -90) {
+						transform.rotation = new Quaternion(0, 0, -transform.rotation.z, transform.rotation.w);
+					} else {
+						transform.rotation = new Quaternion(0, 0, transform.rotation.z, transform.rotation.w);
+					}
+					transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+					transform.position = Vector2.MoveTowards(transform.position, path.lookPoints[pathIndex], speed * Time.deltaTime);
+				}
 
-			yield return null;
+				yield return null;
+			}
 		}
 	}
 
